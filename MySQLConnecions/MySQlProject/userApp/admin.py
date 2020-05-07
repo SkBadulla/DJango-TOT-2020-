86050b4fbc5f79be020260828114a4e4981b41c0
SkBadulla <skbadulla809@gmail.com>
2020-05-07 12:28:06 +0530

    day14 commit

--- MySQLConnecions/MySQlProject/userApp/admin.py
+++ MySQLConnecions/MySQlProject/userApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.site_title='my login page'
+admin.site.site_header = 'my DJango Page'
+admin.site.index_title='index title'
